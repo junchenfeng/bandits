45c38b0f88883a2e8e1ff28a147afbfe43b082ec
Feng Junchen <fengjunchen@huohua.cn>
2020-08-21 21:32:58 +0800

    增加因果推断政策

--- bandits/policy.py
+++ bandits/policy.py
@@ -5,8 +5,9 @@ class Policy(object):
     """
     A policy prescribes an action to be taken based on the memory of an agent.
     """
+
     def __str__(self):
-        return 'generic policy'
+        return "generic policy"
 
     def choose(self, agent):
         return 0
@@ -19,11 +20,12 @@ class EpsilonGreedyPolicy(Policy):
     multiple actions are tied for best choice, then a random action from that
     subset is selected.
     """
+
     def __init__(self, epsilon):
         self.epsilon = epsilon
 
     def __str__(self):
-        return '\u03B5-greedy (\u03B5={})'.format(self.epsilon)
+        return "\u03B5-greedy (\u03B5={})".format(self.epsilon)
 
     def choose(self, agent):
         if np.random.random() < self.epsilon:
@@ -48,11 +50,12 @@ class GreedyPolicy(EpsilonGreedyPolicy):
     random selection. This can be seen as a special case of EpsilonGreedy where
     epsilon = 0 i.e. always exploit.
     """
+
     def __init__(self):
         super(GreedyPolicy, self).__init__(0)
 
     def __str__(self):
-        return 'greedy'
+        return "greedy"
 
 
 class RandomPolicy(EpsilonGreedyPolicy):
@@ -61,11 +64,12 @@ class RandomPolicy(EpsilonGreedyPolicy):
     consideration to which is apparently best. This can be seen as a special
     case of EpsilonGreedy where epsilon = 1 i.e. always explore.
     """
+
     def __init__(self):
         super(RandomPolicy, self).__init__(1)
 
     def __str__(self):
-        return 'random'
+        return "random"
 
 
 class UCBPolicy(Policy):
@@ -74,16 +78,17 @@ class UCBPolicy(Policy):
     factor to the expected value of each arm which can influence a greedy
     selection strategy to more intelligently explore less confident options.
     """
+
     def __init__(self, c):
         self.c = c
 
     def __str__(self):
-        return 'UCB (c={})'.format(self.c)
+        return "UCB (c={})".format(self.c)
 
     def choose(self, agent):
-        exploration = np.log(agent.t+1) / agent.action_attempts
+        exploration = np.log(agent.t + 1) / agent.action_attempts
         exploration[np.isnan(exploration)] = 0
-        exploration = np.power(exploration, 1/self.c)
+        exploration = np.power(exploration, 1 / self.c)
 
         q = agent.value_estimates + exploration
         action = np.argmax(q)
@@ -100,8 +105,9 @@ class SoftmaxPolicy(Policy):
     then randomly samples from the resultant distribution. This policy is
     primarily employed by the Gradient Agent for learning relative preferences.
     """
+
     def __str__(self):
-        return 'SM'
+        return "SM"
 
     def choose(self, agent):
         a = agent.value_estimates
@@ -109,3 +115,37 @@ class SoftmaxPolicy(Policy):
         cdf = np.cumsum(pi)
         s = np.random.random()
         return np.where(s < cdf)[0][0]
+
+
+class FrequentistPolicy(Policy):
+    """
+    Frequentist policy used a fixed length for pure exploration then the rest for pure exploitation
+    """
+
+    def __str__(self):
+        return "Freq"
+
+    def __init__(self, n):
+        self.N = n
+        self.n = 0
+        self._last_choice = None
+        self._optimal_choice = None
+
+    def choose(self, agent):
+        if self.n < self.N:
+            if self._last_choice is None:
+                this_choice = 0
+            else:
+                # absolute even distribution
+                this_choice = (
+                    self._last_choice + 1
+                    if self._last_choice != max(agent.value_estimates.shape)
+                    else 0
+                )
+            return this_choice
+        elif self.n == self.N:
+            # not really a t-test. It saves the trouble of indecision. The intuition still holds
+            self._optimal_choice = np.argmax(agent.value_estimates)
+            return self._optimal_choice
+        else:
+            return self._optimal_choice
